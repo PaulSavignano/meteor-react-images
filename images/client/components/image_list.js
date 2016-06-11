1ea5cde43bc494a8307d789c87d9a970112eee22
Paul Savignano <Paul.Savignano@gmail.com>
2016-06-11 15:00:08 -0700

    Added image detail and list components.

---
+++ images/client/components/image_list.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  {title: 'Pen', link: 'http://dummyimage.com/600x400'},
+  {title: 'Pine Tree', link: 'http://dummyimage.com/600x400'},
+  {title: 'Mug', link: 'http://dummyimage.com/600x400'},
+];
+
+const ImageList = () => {
+  const RenderedImages = IMAGES.map(function(image) {
+    return <ImageDetail image={image} />
+  });
+  return (
+    <ul className="media-list list-group">
+      {RenderedImages}
+    </ul>
+  );
+};
+
+export default ImageList;
