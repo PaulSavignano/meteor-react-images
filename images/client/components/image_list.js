26fd0c0a08618b0d9a932b14c3a9a78545ec5f4b
Paul Savignano <Paul.Savignano@gmail.com>
2016-06-12 09:19:12 -0700

    Added state to render ajax response when ready.

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ImageDetail from './image_detail';
 
+
 const IMAGES = [
   {title: 'Pen', link: 'http://dummyimage.com/600x400'},
   {title: 'Pine Tree', link: 'http://dummyimage.com/600x400'},
@@ -8,9 +9,9 @@ const IMAGES = [
 ];
 
 const ImageList = () => {
-  const RenderedImages = IMAGES.map(function(image) {
-    return <ImageDetail image={image} />
-  });
+  const RenderedImages = IMAGES.map(image =>
+    <ImageDetail key={image.title} image={image} />
+  );
   return (
     <ul className="media-list list-group">
       {RenderedImages}
--- images/client/main.js
+++ images/client/main.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ImageList from './components/image_list';
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <div>
-      <ImageList />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { images: [] };
+  }
+
+  componentWillMount() {
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+      .then(response => this.setState({ images: response.data.data }));
+  }
+
+  render() {
+    console.log(this.state.images)
+    return (
+      <div>
+        <ImageList />
+      </div>
+    );
+  }
 };
+
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
